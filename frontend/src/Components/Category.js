7576b89847e58a8670e197afd82cdf950e5d9a29
diganta413 <ghoshdiganta226@gmail.com>
2021-06-28 17:19:40 +0530

    Filters added

--- frontend/src/Components/Category.js
+++ frontend/src/Components/Category.js
@@ -14,40 +14,117 @@ import ProductInfo from "./ProductInfo";
 
 function Category() {
     const { category } = useParams();
-    //const [products,setproducts] = useState();
     const [value,setvalue] = useState(20000)
     const [min,setmin] = useState(0)
     const [max,setmax] = useState(20000)
     const [sort,setsort] = useState()
     const dispatch = useDispatch()
     const { loading,productList } = useSelector(state => state.products)
-    const [cat_prod,setcat_prod] = useState([])
-    console.log(category)
+    const [cat_prod,setcat_prod] = useState()
+    const [change,setchange] = useState(false)
 
     useEffect(() => {
         
             dispatch(get_all_products());
-            
-            productList?.map(prod => {
-            if(prod.category == category)
+            var new_list = []
+            productList?.forEach(prod => {
+            if(prod?.category == category)
             {
-                setcat_prod([...cat_prod,prod])
+                console.log(prod)
+                new_list.push(prod)
             }
+            setcat_prod(new_list)
+            //console.log(productList)
         });
         
-        
-        
-    
-        /*const get_all = async () => {
-            await axios.get(`http://127.0.0.1:5000/products/${category}`)
-            .then((res) => setproducts(res.data))
-            .catch((err) => console.log(err))
-        }*/
 
         
         
     }, [])
 
+
+    useEffect(() => {
+        console.log(min)
+        console.log(max)
+    },[])
+
+    function sort_by_price(a,b){
+        if ( parseInt(a.Price) < parseInt(b.Price)){
+            return -1;
+          }
+          if ( parseInt(a.Price) > parseInt(b.Price) ){
+            return 1;
+          }
+          return 0;
+        }
+
+        function sort_by_rating(a,b){
+            if ( parseFloat(a.rating) < parseFloat(b.rating)){
+                return -1;
+              }
+              if ( parseFloat(a.rating) > parseFloat(b.rating) ){
+                return 1;
+              }
+              return 0;
+        }
+        function sort_by_reviews(a,b){
+            if ( parseInt(a.reviews) < parseInt(b.reviews)){
+                return -1;
+              }
+              if ( parseInt(a.reviews) > parseInt(b.reviews) ){
+                return 1;
+              }
+              return 0;
+        }
+    
+
+    const sort_changed = (e) => {
+        var prods = cat_prod;
+        if(e.target.value == "By Price")
+        {
+        prods.sort(sort_by_price)
+        }
+        if(e.target.value == "By Rating")
+        {
+            prods.sort(sort_by_rating)
+        }
+        if(e.target.value == "By Total Reviews")
+        {   
+            prods.sort(sort_by_reviews)
+        }
+        setcat_prod(prods)
+        setchange(!change)
+    }
+
+    const slider_changed = (e) => {
+        setvalue(e.target.value)
+        const max = parseInt(e.target.value)
+        var new_list = []
+        productList.forEach((prod) => {
+            if((parseInt(prod.Price) <= max) && (prod.category == category))
+            {
+                console.log(prod)
+                new_list.push(prod)
+            }
+        })
+        setcat_prod(new_list)
+        setchange(!change)
+    }
+
+    const slided = () => {
+        const max = parseInt(value)
+        var new_list = []
+        productList.forEach((prod) => {
+            if((parseInt(prod.Price) <= max) && (prod.category == category))
+            {
+                console.log(prod)
+                new_list.push(prod)
+            }
+        })
+        setcat_prod(new_list)
+        setchange(!change)
+    }
+ 
     
 
     return (
@@ -57,20 +134,19 @@ function Category() {
                 <hr/>
                 <div>
                     <h3 style={{ fontWeight: "bolder",marginLeft: "20px" }}>Price</h3>
-                    <Slider value={value} max={max} min={min} onChange={(e,newvalue) => setvalue(newvalue)} style={{ width: "80%",marginLeft: "10%" }}/>
+                    <Slider value={value} max={max} min={min} onChange={(e,newvalue) => setvalue(newvalue)} onChangeCommitted={slided} style={{ width: "80%",marginLeft: "10%" }}/>
                     <div style={{ display: "flex",alignItems: "center",justifyContent: "center",maxWidth: "80%",marginLeft: "5%" }}>
                         <p style={{ margin: "5%" }}>$</p>
                         <input value={min} onChange={(e) => setmin(e.target.value)} style={{ width: "80px",outline: "none",borderRadius: "5px" }}></input>
                         <p style={{ margin: "0 10px" }}>to</p>
-                        <input value={value} onChange={(e) => setvalue(e.target.value)} style={{ width: "80px",outline: "none",borderRadius: "5px" }}></input>
+                        <input value={value} onChange={slider_changed} style={{ width: "80px",outline: "none",borderRadius: "5px" }}></input>
                     </div>
                     <FormControl component="fieldset" style={{ margin: "10%" }}>
                         <FormLabel component="legend" style={{ color: "black",fontWeight: "bolder" }}>Sort By</FormLabel>
-                            <RadioGroup aria-label="gender" name="gender1" value={sort} onChange={(e) => setsort(e.target.value)}>
+                            <RadioGroup aria-label="gender" name="gender1" value={sort} onChange={sort_changed}>
                                 <FormControlLabel value="By Rating" control={<Radio color="primary"/>} label="By Rating" />
                                 <FormControlLabel value="By Price" control={<Radio color="primary"/>} label="By Price" />
                                 <FormControlLabel value="By Total Reviews" control={<Radio color="primary"/>} label="By Total Reviews" />
-                                <FormControlLabel value="None" control={<Radio color="primary"/>} label="None" />
                             </RadioGroup>
                     </FormControl>
                 </div>
@@ -81,7 +157,7 @@ function Category() {
                 <div style={{ marginTop: "10px",display: "grid",gridTemplateColumns: "repeat(5, 1fr)",gridGap: "20px" }}>
                 {loading?
                 (<CircularProgress style={{ margin: "20%",marginLeft: "60%" }}/>):
-                (productList?.map((prod) => (
+                (cat_prod?.map((prod) => (
                     (prod.category == category)?
                     (<ProductInfo product={prod}/>):
                     ""
